307127d2b2fcf2d3378f6d3ffd866c454d7c22aa
Alexander Ofuya <esigbemi@gmail.com>
2021-12-24 16:39:07 +0000

    first update

--- Jenkins.sh
+++ Jenkins.sh
@@ -9,13 +9,12 @@ Install Java JDK 1.8+ as Jenkins pre-requisit
 Install other softwares - git, unzip and wget
 Prerequisite
 
-hostname cicd
+sudo hostname Jenkins
 sudo yum -y install unzip wget tree git
 sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
 sudo yum install jdk-8u131-linux-x64.rpm -y
 
 # Add Jenkins Repository and key
-
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 cd /etc/yum.repos.d/
 sudo curl -O https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -29,7 +28,7 @@ sudo systemctl enable jenkins
 sudo systemctl status jenkins
 
 # Access Jenkins from the browser
-public-ip:8080
+# public-ip:8080
 
 # get jenkins initial admin password
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
